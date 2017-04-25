ea7c1bc4e4c630840a3e9a3e544143eef91665f9
mskalbania <cod634.steam@gmail.com>
2017-04-25 19:11:48 +0200

    2.4

--- 2/src/com/sda/Arrays2.java
+++ 2/src/com/sda/Arrays2.java
@@ -4,10 +4,36 @@ public class Arrays2 {
 
     public static void main(String[] args) {
 
+        int[] arr = new int[100];
 
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
 
+        double average = countAverage(arr);
 
+        System.out.println("Av = " + average);
+
+        //  showArray(arr);
 
 
     }
+
+    public static double countAverage(int[] arr) {
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        return (double) (sum) / (double) (arr.length);
+    }
+
+    public static void showArray(int[] arr) {
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+
 }
