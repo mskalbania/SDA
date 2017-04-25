a9ec7387cbeb7e91d347af687dab766edb621745
mskalbania <cod634.steam@gmail.com>
2017-04-25 18:28:41 +0200

    2.3

--- 2/src/com/sda/Arrays.java
+++ 2/src/com/sda/Arrays.java
@@ -2,26 +2,33 @@ package com.sda;
 
 public class Arrays {
 
-    private final static int rows = 5;
-    private final static int columns = 5;
+    private final static int x = 7;
+    private final static int y = 7;
+    private final static int z = 7;
 
     public static void main(String[] args) {
 
-        int[][] arr = new int[rows][columns];
+        int[][][] arr = new int[x][y][z];
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                arr[i][j] = i + j;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                for (int k = 0; k < z; k++) {
+                    arr[x][y][z] = i + j + k;
+                }
             }
         }
 
-        for (int[] i : arr) {
+        for (int[][] i : arr) {
 
-            for (int j : i) {
-                System.out.print(j + " ");
+            for (int[] j : i) {
+
+                for (int k:j){
+                    System.out.println(k);
+                }
+                System.out.println();
             }
+            System.out.println();
 
-            System.out.println("");
         }
 
     }
