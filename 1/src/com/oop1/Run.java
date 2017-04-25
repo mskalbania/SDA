d367388caa9e50946d765d4a4d56cc1021227080
mskalbania <cod634.steam@gmail.com>
2017-04-25 20:57:33 +0200

    2.0

--- 1/src/com/oop1/Run.java
+++ 1/src/com/oop1/Run.java
@@ -1,17 +1,33 @@
 package com.oop1;
 
+/*
+Random cars (objects) generating and adding them to list (garage)
+ */
+
 public class Run {
 
     public static void main(String[] args) {
 
+        final String[] carManufacturers = {"Ferrari", "Audi", "Porsche", "Toyota",
+                                            "Fiat", "BMW", "Mercedes"};
+        final double[] carEngineSizes = {1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0};
+
+        final String[] carColours = {"red", "white", "black", "yellow", "orange"};
+
+        final int numberOfCarsAdded = 5;
+
         Garage garage1 = new Garage();
 
-        garage1.addCarToGarage("Ferrari", 4.0, "red");
-        garage1.addCarToGarage("Porsche", 3.0, "yellow");
-        garage1.addCarToGarage("Audi", 2.0, "grey");
+        for (int i = 0; i < numberOfCarsAdded ; i++) {
+            garage1.addCarToGarage(carManufacturers[(int)(Math.random()*7)],
+                                    carEngineSizes[(int)(Math.random()*7)],
+                                    carColours[(int)(Math.random()*5)]);
+        }
 
         garage1.showListOfCars();
     }
+
+
 
 
 }
