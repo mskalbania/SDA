17bee6242a963a368ddfa111a74d0356eb98f6d8
mskalbania <cod634.steam@gmail.com>
2017-04-26 17:27:27 +0200

    a

---
+++ 1/src/a.java
@@ -0,0 +1,17 @@
+public class a {
+    public static void main(String[] args) {
+        int[] tablica = new int[100];
+
+        for (int i = 0; i < 100; i++) {
+            tablica[i] = i;
+        }
+
+        for(int x:tablica){
+            System.out.println(x);
+        }
+
+
+    }
+
+
+}
