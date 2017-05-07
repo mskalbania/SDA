16a73a247ed89e6b4a42c0d9c072473f04c07967
mskalbania <cod634.steam@gmail.com>
2017-05-07 20:34:16 +0200

    DB test

---
+++ TestDB/src/org/database/TestDB.java
@@ -0,0 +1,21 @@
+package org.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestDB {
+
+    private final static String CONNECTION_URL = "jdbc:sqlite:/home/mateusz/Desktop/SDA/TestDB/databases/firstDB";
+
+    public static void main(String[] args) {
+
+        try (Connection connection = DriverManager.getConnection(CONNECTION_URL);
+             Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts(name TEXT, phone INTEGER, email TEXT)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
